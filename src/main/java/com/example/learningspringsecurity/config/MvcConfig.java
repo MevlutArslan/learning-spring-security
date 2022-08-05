d76e11d054e2be6fe1edf0f3c968fc79c3ab3ca7
MevlutArslan <mevlut12098@gmail.com>
2022-08-05 16:16:42 +0200

    add Bean for password encoder.

--- src/main/java/com/example/learningspringsecurity/config/MvcConfig.java
+++ src/main/java/com/example/learningspringsecurity/config/MvcConfig.java
@@ -1,11 +1,18 @@
 package com.example.learningspringsecurity.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     public void addViewControllers(ViewControllerRegistry registry){
         registry.addViewController("/home").setViewName("home");
